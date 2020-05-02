90996ace233d886c8ae0361257cb42f207ed284a
roshane <roshanemadusanka@gmail.com>
2020-05-02 18:51:11 +0800

    migrated to gradle complete

--- src/intTest/java/com/aeon/scheduler/service/AbstractBaseIntTest.java
+++ src/intTest/java/com/aeon/scheduler/service/AbstractBaseIntTest.java
@@ -1,4 +1,4 @@
-package com.aeon.scheduler.service.it;
+package com.aeon.scheduler.service;
 
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Timeout;
@@ -7,6 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Tag("IntegrationTest")
 @Timeout(5)
 @SpringBootTest
-public abstract class AbstractBase_ItTest {
+public abstract class AbstractBaseIntTest {
 }
 
