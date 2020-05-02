aacc9debb6b7153ef91bf26265b487752c7d0985
roshane <roshanemadusanka@gmail.com>
2020-05-03 00:37:12 +0800

    minor fixes

--- src/intTest/java/com/aeon/scheduler/service/controller/SystemControllerIntTest.java
+++ src/intTest/java/com/aeon/scheduler/service/controller/SystemControllerIntTest.java
@@ -1,15 +1,15 @@
 package com.aeon.scheduler.service.controller;
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.aeon.scheduler.service.AbstractBaseIntTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
-
-import static org.hamcrest.Matchers.containsString;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 class SystemControllerIntTest extends AbstractBaseIntTest {
--- src/test/java/com/aeon/scheduler/service/controller/SystemControllerTest.java
+++ src/test/java/com/aeon/scheduler/service/controller/SystemControllerTest.java
@@ -1,18 +1,18 @@
 package com.aeon.scheduler.service.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.aeon.scheduler.service.AbstractBaseUnitTest;
 import com.aeon.scheduler.service.http.response.HealthResponse;
 import com.aeon.scheduler.service.service.HealthService;
-import com.aeon.scheduler.service.AbstractBaseUnitTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
 
 @Tag("controller")
 class SystemControllerTest extends AbstractBaseUnitTest {
-    private final HealthService mockHealthService = mock(HealthService.class);
+    private final HealthService mockHealthService = Mockito.mock(HealthService.class);
 
     private SystemController createClassUnderTest() {
         return new SystemController(mockHealthService);
@@ -23,8 +23,8 @@ class SystemControllerTest extends AbstractBaseUnitTest {
     void test_system_controller_returns_200ok() throws Exception {
         String fakeTimestamp = "2020-04-04T21:18:14";
         HealthResponse fakeResponse = new HealthResponse(fakeTimestamp, "1.0.0", "healthy", activeProfile);
-        when(mockHealthService.getHealth()).thenReturn(fakeResponse);
+        Mockito.when(mockHealthService.getHealth()).thenReturn(fakeResponse);
         assertEquals(fakeResponse.toResponseString(), mockHealthService.getHealth().toResponseString());
-        verify(mockHealthService).getHealth();
+        Mockito.verify(mockHealthService).getHealth();
     }
 }
