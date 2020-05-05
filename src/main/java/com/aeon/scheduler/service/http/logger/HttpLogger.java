639b654cb7e6dc9c7007dd22c9ad8944f7e76f54
roshane <roshanemadusanka@gmail.com>
2020-05-05 23:48:20 +0800

    added docker support for travis build

--- src/main/java/com/aeon/scheduler/service/http/logger/HttpLogger.java
+++
@@ -1,4 +0,0 @@
-package com.aeon.scheduler.service.http.logger;
-
-public class HttpLogger {
-}
---
+++ src/main/java/com/aeon/scheduler/service/util/HttpLogger.java
@@ -0,0 +1,9 @@
+package com.aeon.scheduler.service.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HttpLogger {
+    private static final Logger logger = LoggerFactory.getLogger(HttpLogger.class);
+
+}
